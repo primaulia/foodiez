32f3aca254f4b7cfff65af4f10e60e997a62e7eb
Prima Aulia Gusta <primaulia@gmail.com>
2017-10-18 11:48:07 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,164 @@
+// setting all global variables (note: why const? cos it won't change)
+// notice that port for mongodb is not really needed
+const dbUrl = 'mongodb://localhost/test'
+const port = 4000 // this is for our express server
+
+// installing all modules
+const express = require('express')
+const path = require('path') // for Public files
+const mongoose = require('mongoose') // for DB
+const exphbs = require('express-handlebars') // for Handlebars
+const bodyParser = require('body-parser') // for accessing POST request
+const methodOverride = require('method-override') // for accessing PUT / DELETE
+
+// requiring actual file now
+const Restaurant = require('./models/Restaurant')
+
+// initiating express, by calling express variable
+const app = express()
+
+// VIEW ENGINES aka handlebars setup
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+// MIDDLEWARES (explained on thursday)
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(function (req, res, next) {
+  console.log('Method: ' + req.method + ' Path: ' + req.url)
+  next()
+})
+// setup bodyParser
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+// setup methodOverride
+app.use(methodOverride('_method'))
+
+// connecting to mongodb before we starting the server
+// via mongoose
+mongoose.Promise = global.Promise // the formidable Promise, so we can use .then()
+mongoose.connect(dbUrl, {
+  // this means that technically mongoose use the same technique
+  // like MongoClient.connect
+  useMongoClient: true
+}) // http://mongoosejs.com/docs/connections.html
+.then(
+  () => { console.log('db is connected') },
+  (err) => { console.log(err) }
+)
+
+// ROUTE sections
+// note: remember all the model file we created on models/restaurant.js,
+// we'll use it again
+
+// READ ALL
+app.get('/', (req, res) => {
+  // the return of then
+  Restaurant.find().limit(10)
+  .then(restaurants => {
+    // at this point we got our data so we can render our page
+
+    res.render('home', {
+      restaurants
+      // remember object literal on es6, we don't need to type in pairs
+      // if key and argument is the same name
+      // i.e. restaurants: restaurants
+    })
+  })
+  .catch(err => {
+    console.log(err)
+  })
+})
+
+// FORM PAGE (NOT WITHIN CRUD)
+// note: this route must be before '/restaurants/:id'. WHY?
+app.get('/restaurants/new', (req, res) => {
+  res.render('restaurants/new')
+})
+
+// READ ONE
+app.get('/restaurants/:id', (req, res) => {
+  // instead of find all, we can `findById`
+  Restaurant.findById(req.params.id) // no need limit since there's only one
+  .then(restaurant => {
+ // not restaurants, cos it's single restaurant
+
+    // PITSTOP: look at the views folders here, compare it with the res.render
+    // first argument
+
+    res.render('restaurants/show', {
+      restaurant
+    })
+  })
+  .catch(err => {
+    console.log(err)
+  })
+})
+
+// CREATE ONE
+// pseudocode
+// get the value from the form
+// create new object based on that form
+app.post('/restaurants', (req, res) => {
+  var formData = req.body
+
+  // remember how to create new object from constructor, it's back again
+  // thanks to FORMIDABLE mongoose
+  var newRestaurant = new Restaurant()
+  newRestaurant.name = formData.name
+  newRestaurant.cuisine = formData.cuisine
+
+  // when save function is done
+  // the newRestaurant will have an id, hence we can go straight to the
+  // newly created restaurant page
+  newRestaurant.save()
+  .then(() => res.redirect(`/restaurants/${newRestaurant.id}`))
+  .catch(err => console.log(err))
+})
+
+// UPDATE ONE
+// pseudocode
+// get the id from the url
+// get the updated value from form
+// find restaurant object by id given
+// update with the input from form
+app.put('/restaurants/:id', (req, res) => {
+  // thankfully since we're using mongoose
+  // we don't have to find and update separately
+  // there's a method in mongoose just for that
+  // `findByIdAndUpdate` http://mongoosejs.com/docs/api.html#model_Model.findByIdAndUpdate
+
+  var formData = req.body
+  Restaurant.findByIdAndUpdate(req.params.id, {
+    name: formData.name,
+    cuisine: formData.cuisine
+  })
+  .then(() => res.redirect(`/restaurants/${req.params.id}`))
+  .catch(err => console.log(err))
+  // after update is done, redirect back to resto id
+  // this redirection can go to anywhere as long as you have the routes with you
+})
+
+// DELETE ONE
+// pseudocode
+// get the id from the url
+// find restaurant object by id given
+// delete the whole object
+app.delete('/restaurants/:id', (req, res) => {
+  // (AGAIN) thankfully since we're using mongoose
+  // there's a method in mongoose just for that
+  // `findByIdAndRemove` http://mongoosejs.com/docs/api.html#model_Model.findByIdAndRemove
+
+  Restaurant.findByIdAndRemove(req.params.id)
+  .then(() => res.redirect(`/`))
+  .catch(err => console.log(err))
+  // after delete is done, redirect back to home page
+  // (cos the current restaurant page is gone)
+  // this redirection can go to anywhere as long as you have the routes with you
+})
+
+// opening the port for express
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`)
+})
---
+++ models/restaurant.js
@@ -0,0 +1,45 @@
+// what is this file for? I'll explain on thursday
+// for the curious read here https://jeremiahalex.gitbooks.io/wdi-sg/content/05-express/express-mongoose/readme.html
+
+// we need to initiate mongoose again here, because technically
+// we don't run our node here, we run our node in index.js
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema // http://mongoosejs.com/docs/guide.html
+
+// create a schema
+// remember your ERD, this is where the diagram is your help to write the code
+const restaurantSchema = new Schema({
+  // <fields name>: <type> OR
+
+  // <fields name>: {
+  //   type: <type>,
+  //   other stuff I'll explain
+  // }
+
+  // compare the structure here with your restaurants data
+
+  address: {
+    building: String,
+    coord: Array,
+    street: String,
+    zipcode: String
+  },
+  borough: String,
+  cuisine: String,
+  grades: [{
+    date: Date,
+    grade: String,
+    score: Number
+  }],
+  name: String,
+  restaurant_id: String
+})
+
+// Think that the line below here
+// is like when you're building a blueprint with `restaurantSchema`
+// and you're now creating an object based on that blueprint
+const Restaurant = mongoose.model('Restaurant', restaurantSchema)
+
+// make this available to our other files
+// remember that technically we're only running index.js
+module.exports = Restaurant
