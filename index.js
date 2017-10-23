9aa066cea750f6a066cfa55443e8025f9a9e8d72
Prima Aulia Gusta <primAulia@gmail.com>
2017-10-23 10:35:16 +0800

    latest port setup

--- index.js
+++ index.js
@@ -45,7 +45,7 @@
 // setting all global variables (note: why const? cos it won't change)
 // notice that port for mongodb is not really needed
 const dbUrl = 'mongodb://localhost/test'
-const port = 4000 // this is for our express server
+const port = process.env.PORT || 4000 // this is for our express server
 
 // installing all modules
 const express = require('express')
