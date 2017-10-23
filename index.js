55c56b35fdbadd60d357325ed88f783ada00db9d
Prima Aulia Gusta <primAulia@gmail.com>
2017-10-23 10:44:51 +0800

    setup mlab for heroku

--- index.js
+++ index.js
@@ -44,7 +44,7 @@
 
 // setting all global variables (note: why const? cos it won't change)
 // notice that port for mongodb is not really needed
-const dbUrl = 'mongodb://localhost/test'
+const dbUrl = process.env.MONGODB_URI || 'mongodb://localhost/test'
 const port = process.env.PORT || 4000 // this is for our express server
 
 // installing all modules
